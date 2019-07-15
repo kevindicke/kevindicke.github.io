766860cb57df1b11132b0469ee073d90b8eccfec
Kevin Dicke <k.dicke7@gmail.com>
2019-07-15 13:36:44 -0700

    Updates

--- precache-manifest.945de51748379d0c4ff4bc3ec92706c5.js
+++ precache-manifest.945de51748379d0c4ff4bc3ec92706c5.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3a7051caf9df4cd3aa65306b9f708548",
+    "revision": "1476beb1dcee4f33e06c60b3c2aa47f3",
     "url": "/index.html"
   },
   {
-    "revision": "4b623e8593c9ac990379",
+    "revision": "34716108484363261769",
     "url": "/static/css/main.2cce8147.chunk.css"
   },
   {
@@ -12,15 +12,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.b41502e9.chunk.js"
   },
   {
-    "revision": "4b623e8593c9ac990379",
-    "url": "/static/js/main.28647029.chunk.js"
+    "revision": "34716108484363261769",
+    "url": "/static/js/main.497f349a.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
     "url": "/static/js/runtime~main.a8a9905a.js"
   },
   {
-    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
-    "url": "/static/media/logo.5d5d9eef.svg"
+    "revision": "7928c669b1400b7d341e4854bc0fc0d2",
+    "url": "/static/media/computer.7928c669.svg"
   }
 ]);
