309a95e8b9556e84d8d6e301ec8a3a62dc13542d
Kevin Dicke <k.dicke7@gmail.com>
2019-07-15 19:12:02 -0700

    Add link to px-vw page

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Kevin Dicke<br/>Master Developer
         </p>
+        <a href="/px-vw">PX to VW Converter</a>
       </header>
     </div>
   );
